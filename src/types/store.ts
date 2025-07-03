69608ef2e73ec5c27c4628a8d08013fe07756910
hyoussef@ridgeco.tech <hyoussef@ridgeco.tech>
2025-07-03 11:14:22 +0300

    [CLEAR] clear project

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,28 +1,3 @@
-// import type { ILoginRequest } from "./auth";
-
-// export interface AuthState {
-//   token: string | null;
-//   isAuthenticated: boolean;
-//   firstName?: string;
-//   lastName?: string;
-//   profileImageURL?: string;
-//   email?: string | undefined;
-//   roleNames?: string[];
-//   permissions?: any;
-//   tempEmail: string;
-//   tempPassword: string | null;
-//   mfa: boolean;
-
-//   login: (userData: any, token: string) => void;
-//   logout: () => void;
-//   setTempEmail: (email: string) => void;
-//   setMfa: (mfa: boolean) => void;
-//   setTempPassword: (password: string) => void;
-//   skipMfa: (email: string, password: string) => Promise<boolean>;
-//   initialize: () => void;
-// }
-
-
 export interface AuthState {
   token: string | null;
   isAuthenticated: boolean;
